eb6fd0a983e80920eb333a991111f3a6f0be0121
aditya <rawatas0901@gmail.com>
2023-05-27 23:05:21 +0530

    logged_in cookie bug

--- backend/controllers/auth.contoller.js
+++ backend/controllers/auth.contoller.js
@@ -101,7 +101,6 @@ module.exports.loginHandler = async (req, res, next) => {
     res.cookie("logged_in", true, {
       ...accessTokenCookieOptions,
       httpOnly: false,
-      secure: false,
     });
 
     // Send Access Token
